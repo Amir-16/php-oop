5507cafbc6f802b2a951dc781427a8b47e409c29
Amir-16 <greenfitter516@gmail.com>
2021-02-19 20:20:36 +0600

    inheritance

---
+++ inheritance/Inherit.php
@@ -0,0 +1,28 @@
+<?php
+
+class University{
+
+	public $uniName="Berlin University";
+	function uni(){
+		echo "Welcome to $this->uniName".PHP_EOL;
+	}
+
+}
+
+
+class Professor extends University{
+
+	public $ProfessorName ="Smith";
+
+	function prof(){
+		echo "Hlw from $this->ProfessorName Professor of Computer Science Department".PHP_EOL;
+	}
+
+
+}
+
+$prof1 =new Professor();
+$prof1->uni();
+$prof1->prof();
+
+echo $prof1->uniName.PHP_EOL;
