94337fe7057ad4fb7f015671403be2904d3d166f
Amir-16 <greenfitter516@gmail.com>
2021-02-24 09:49:01 +0600

    inheritance

---
+++ inheritance/practice.php
@@ -0,0 +1,15 @@
+<?php 
+   
+// Input section 
+// $a = 10 
+$a = (int)readline('Enter an integer: '); 
+  
+// $b = 9.78 
+$b = (float)readline('Enter a floating'
+            . ' point number: '); 
+  
+// Entered integer is 10 and 
+// entered float is 9.78 
+echo "Entered integer is " . $a 
+    . " and entered float is " . $b; 
+?> 
