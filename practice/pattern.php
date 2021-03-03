6503ecb2332d6eeda30f177806d7a56642e294bb
Your Name <greenfitter516@gmail.com>
2021-03-04 01:49:20 +0600

    pattern

---
+++ practice/pattern.php
@@ -0,0 +1,26 @@
+<?php
+
+
+for ($i=1; $i <=5 ; $i++) { 
+	for ($j=0; $j <$i ; $j++) { 
+		echo " $i ";
+	}
+	echo " </br>";
+}
+
+
+for ($i=1; $i <=5 ; $i++) { 
+	for ($j=1; $j <=$i ; $j++) { 
+		echo " * ";
+	}
+	echo " </br>";
+}
+
+for ($i=1; $i <=5 ; $i++) { 
+	for ($j=1; $j <=$i ; $j++) { 
+		echo " $j ";
+	}
+	echo " </br>";
+}
+
+?>
