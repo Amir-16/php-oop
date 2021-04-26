e519b1790de426875210b860c997faee99ce1092
Your Name <greenfitter516@gmail.com>
2021-04-26 20:02:55 +0600

    class

---
+++ class&method/practice.php
@@ -0,0 +1,23 @@
+<?php
+
+class Practice{
+
+
+	public $name;
+	public $designation;
+
+	// function salary($name,$designation){
+
+	// 	$this->name=$name;
+	// 	$this->designation=$designation;
+	// 	return salary;
+	// }
+}
+
+
+$employee= new Practice();
+
+$employee->name ="Amir";
+
+
+echo " Developer $employee->name ";
